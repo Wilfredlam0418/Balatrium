72800691864c88e92773aee6a9df13adc1bb4e6b
Wilfredlam0418 <128967273+Wilfredlam0418@users.noreply.github.com>
2025-06-26 19:36:14 +0800

    almost forgot

--- localization/en-us.lua
+++ localization/en-us.lua
@@ -34,83 +34,83 @@ return {
 			},
 			j_bium_exuberant = {
 				name = "Exuberant Joker",
-				text = {"Increase the {C:acid}Acidity{} of all {C:acid}Acids{}", "by {C:attention}#1#{} if played hand contains", "a {C:attention}Pair"}
+				text = {"Increase the {C:acid}Acidity{} of all {C:acid}Acids", "by {C:attention}#1#{} if played hand contains", "a {C:attention}Pair"}
 			},
 			j_bium_exultant = {
 				name = "Exultant Joker",
-				text = {"Increase the {C:acid}Acidity{} of all {C:acid}Acids{}", "by {C:attention}#1#{} if played hand contains", "a {C:attention}Three of a Kind"}
+				text = {"Increase the {C:acid}Acidity{} of all {C:acid}Acids", "by {C:attention}#1#{} if played hand contains", "a {C:attention}Three of a Kind"}
 			},
 			j_bium_rapturous = {
 				name = "Rapturous Joker",
-				text = {"Increase the {C:acid}Acidity{} of all {C:acid}Acids{}", "by {C:attention}#1#{} if played hand contains", "a {C:attention}Two Pair"}
+				text = {"Increase the {C:acid}Acidity{} of all {C:acid}Acids", "by {C:attention}#1#{} if played hand contains", "a {C:attention}Two Pair"}
 			},
 			j_bium_blithe = {
 				name = "Blithe Joker",
-				text = {"Increase the {C:acid}Acidity{} of all {C:acid}Acids{}", "by {C:attention}#1#{} if played hand contains", "a {C:attention}Straight"}
+				text = {"Increase the {C:acid}Acidity{} of all {C:acid}Acids", "by {C:attention}#1#{} if played hand contains", "a {C:attention}Straight"}
 			},
 			j_bium_ecstatic = {
 				name = "Ecstatic Joker",
-				text = {"Increase the {C:acid}Acidity{} of all {C:acid}Acids{}", "by {C:attention}#1#{} if played hand contains", "a {C:attention}Flush"}
+				text = {"Increase the {C:acid}Acidity{} of all {C:acid}Acids", "by {C:attention}#1#{} if played hand contains", "a {C:attention}Flush"}
 			},
 			j_bium_thrilled = {
 				name = "Thrilled Joker",
-				text = {"Increase the {C:acid}Acidity{} of all {C:acid}Acids{}", "by {C:attention}#1#{} if played hand contains", "a {C:attention}High Card"}
+				text = {"Increase the {C:acid}Acidity{} of all {C:acid}Acids", "by {C:attention}#1#{} if played hand contains", "a {C:attention}High Card"}
 			},
 			j_bium_enlivened = {
 				name = "Enlivened Joker",
-				text = {"Increase the {C:acid}Acidity{} of all {C:acid}Acids{}", "by {C:attention}#1#{} if played hand contains", "a {C:attention}Four of a Kind"}
+				text = {"Increase the {C:acid}Acidity{} of all {C:acid}Acids", "by {C:attention}#1#{} if played hand contains", "a {C:attention}Four of a Kind"}
 			},
 			j_bium_enthusiastic = {
 				name = "Enthusiastic Joker",
-				text = {"Increase the {C:acid}Acidity{} of all {C:acid}Acids{}", "by {C:attention}#1#{} if played hand contains", "a {C:attention}Straight Flush"}
+				text = {"Increase the {C:acid}Acidity{} of all {C:acid}Acids", "by {C:attention}#1#{} if played hand contains", "a {C:attention}Straight Flush"}
 			},
 			j_bium_delighted = {
 				name = "Delighted Joker",
-				text = {"Increase the {C:acid}Acidity{} of all {C:acid}Acids{}", "by {C:attention}#1#{} if played hand contains", "a {C:attention}Full House"}
+				text = {"Increase the {C:acid}Acidity{} of all {C:acid}Acids", "by {C:attention}#1#{} if played hand contains", "a {C:attention}Full House"}
 			},
 			j_bium_adrenalized = {
 				name = "Adrenalized Joker",
-				text = {"Increase the {C:acid}Acidity{} of all {C:acid}Acids{}", "by {C:attention}#1#{} if played hand contains", "a {C:attention}Five of a Kind"}
+				text = {"Increase the {C:acid}Acidity{} of all {C:acid}Acids", "by {C:attention}#1#{} if played hand contains", "a {C:attention}Five of a Kind"}
 			},
 			j_bium_astonishing = {
 				name = "Astonishing Joker",
-				text = {"Increase the {C:acid}Acidity{} of all {C:acid}Acids{}", "by {C:attention}#1#{} if played hand contains", "a {C:attention}Flush House"}
+				text = {"Increase the {C:acid}Acidity{} of all {C:acid}Acids", "by {C:attention}#1#{} if played hand contains", "a {C:attention}Flush House"}
 			},
 			j_bium_staggering = {
 				name = "Staggering Joker",
-				text = {"Increase the {C:acid}Acidity{} of all {C:acid}Acids{}", "by {C:attention}#1#{} if played hand contains", "a {C:attention}Flush Five"}
+				text = {"Increase the {C:acid}Acidity{} of all {C:acid}Acids", "by {C:attention}#1#{} if played hand contains", "a {C:attention}Flush Five"}
 			},
 			j_bium_extraordinary = {
 				name = "Extraordinary Joker",
-				text = {"Increase the {C:acid}Acidity{} of all {C:acid}Acids{}", "by {C:attention}#1#{} if played hand contains", "a {C:attention}Bulwark"}
+				text = {"Increase the {C:acid}Acidity{} of all {C:acid}Acids", "by {C:attention}#1#{} if played hand contains", "a {C:attention}Bulwark"}
 			},
 			j_bium_perplexing = {
 				name = "Perplexing Joker",
-				text = {"Increase the {C:acid}Acidity{} of all {C:acid}Acids{}", "by {C:attention}#1#{} if played hand contains", "a {C:attention}Cluster"}
+				text = {"Increase the {C:acid}Acidity{} of all {C:acid}Acids", "by {C:attention}#1#{} if played hand contains", "a {C:attention}Cluster"}
 			},
 			j_bium_spectacular = {
 				name = "Spectacular Joker",
-				text = {"Increase the {C:acid}Acidity{} of all {C:acid}Acids{}", "by {C:attention}#1#{} if played hand contains", "an {C:attention}Ultimate Pair"}
+				text = {"Increase the {C:acid}Acidity{} of all {C:acid}Acids", "by {C:attention}#1#{} if played hand contains", "an {C:attention}Ultimate Pair"}
 			}
 		},
 		Spectral = {
 			c_bium_balatrium = {
 				name = "Balatrium",
-				text = {"Creates a {C:dark_edition}Negative{} copy", "of every discovered {V:1}#1#{}"}
+				text = {"Creates a {C:dark_edition}Negative{} copy", "of every discovered {V:1}#1#"}
 			}
 		},
 		Element = {
 			c_bium_hydrogen = {
 				name = "Hydrogen",
-				text = {"Converts all selected", "cards to {V:1}#2#{}"}
+				text = {"Converts all selected", "cards to {V:1}#2#"}
 			},
 			c_bium_deuterium = {
 				name = "Deuterium",
-				text = {"Converts all selected", "cards to {V:1}#2#{}"}
+				text = {"Converts all selected", "cards to {V:1}#2#"}
 			},
 			c_bium_tritium = {
 				name = "Tritium",
-				text = {"Converts all selected", "cards to {V:1}#2#{}"}
+				text = {"Converts all selected", "cards to {V:1}#2#"}
 			},
 			c_bium_helium = {
 				name = "Helium",
@@ -118,31 +118,31 @@ return {
 			},
 			c_bium_lithium = {
 				name = "Lithium",
-				text = {"Creates a {C:attention}Battery{}"}
+				text = {"Creates a {C:attention}Battery"}
 			},
 			c_bium_beryllium = {
 				name = "Beryllium",
-				text = {"{C:money}+$#1#{}"}
+				text = {"{C:money}+$#1#"}
 			},
 			c_bium_boron = {
 				name = "Boron",
-				text = {"{C:attention}-#1# Ante{}"}
+				text = {"{C:attention}-#1# Ante"}
 			},
 			c_bium_carbon = {
 				name = "Carbon",
-				text = {"Converts all selected", "cards to {V:1}#2#{}"}
+				text = {"Converts all selected", "cards to {V:1}#2#"}
 			},
 			c_bium_nitrogen = {
 				name = "Nitrogen",
-				text = {"Converts all selected", "cards to {V:1}#2#{}"}
+				text = {"Converts all selected", "cards to {V:1}#2#"}
 			},
 			c_bium_oxygen = {
 				name = "Oxygen",
-				text = {"Converts all selected", "cards to {V:1}#2#{}"}
+				text = {"Converts all selected", "cards to {V:1}#2#"}
 			},
 			c_bium_fluorine = {
 				name = "Fluorine",
-				text = {"Creates an {V:1}#1#{}", "with 0 {C:acid}Acidity{}"}
+				text = {"Creates an {V:1}#1#", "with 0 {C:acid}Acidity"}
 			},
 			c_bium_neon = {
 				name = "Neon",
@@ -150,11 +150,11 @@ return {
 			},
 			c_bium_sodium = {
 				name = "Sodium",
-				text = {"Creates a {C:attention}Salt Cube{}"}
+				text = {"Creates a {C:attention}Salt Cube"}
 			},
 			c_bium_magnesium = {
 				name = "Magnesium",
-				text = {"Multiply {C:money}money{} by {X:money,C:white}X#1#{}"}
+				text = {"Multiply {C:money}money{} by {X:money,C:white}X#1#"}
 			},
 			c_bium_aluminum = {
 				name = "Aluminum",
@@ -166,15 +166,15 @@ return {
 			},
 			c_bium_phosphorus = {
 				name = "Phosphorus",
-				text = {"Creates {C:attention}#1#{} random", "{V:1}#2#s{}"}
+				text = {"Creates {C:attention}#1#{} random", "{V:1}#2#s"}
 			},
 			c_bium_sulfur = {
 				name = "Sulfur",
-				text = {"Redeems a random {C:attention}Voucher{}"}
+				text = {"Redeems a random {C:attention}Voucher"}
 			},
 			c_bium_chlorine = {
 				name = "Chlorine",
-				text = {"Increases the {C:acid}Acidity{} of", "all {C:acid}Acids{} by {C:attention}#1#{}"}
+				text = {"Increases the {C:acid}Acidity{} of", "all {C:acid}Acids{} by {C:attention}#1#"}
 			},
 			c_bium_argon = {
 				name = "Argon",
@@ -182,25 +182,25 @@ return {
 			},
 			c_bium_potassium = {
 				name = "Potassium",
-				text = {"Creates a {C:attention}Gros Michel{}"}
+				text = {"Creates a {C:attention}Gros Michel"}
 			},
 			c_bium_calcium = {
 				name = "Calcium",
-				text = {"{C:money}+$#1#{}, then multiply", "{C:money}money{} by {X:money,C:white}X#2#{}"}
+				text = {"{C:money}+$#1#{}, then multiply", "{C:money}money{} by {X:money,C:white}X#2#"}
 			}
 		},
 		Acid = {
 			c_bium_hf = {
 				name = "Hydrofluoric Acid",
-				text = {"Gives {C:blue}Hands{} and {C:red}Discards{}", "based on the current {C:acid}Acidity{}", "{C:acid}<5{}: {C:blue}0{}/{C:red}0{}", "{C:acid}5-9{}: {C:blue}0{}/{C:red}1{}", "{C:acid}10-14{}: {C:blue}1{}/{C:red}1{}", "{C:acid}>14{}: {C:blue}2{}/{C:red}2{}", "{C:inactive}(Currently {C:acid}#1# Acidity{C:inactive}){}"}
+				text = {"Gives {C:blue}Hands{} and {C:red}Discards", "based on the current {C:acid}Acidity", "{C:acid}<5{}: {C:blue}0{}/{C:red}0", "{C:acid}5-9{}: {C:blue}0{}/{C:red}1", "{C:acid}10-14{}: {C:blue}1{}/{C:red}1", "{C:acid}>14{}: {C:blue}2{}/{C:red}2", "{C:inactive}(Currently {C:acid}#1# Acidity{C:inactive})"}
 			},
 			c_bium_hcl = {
 				name = "Hydrochloric Acid",
-				text = {"Increases {C:acid}Acidity{} of all other", "{C:acid}Acids{} based on the current {C:acid}Acidity{}", "{C:acid}<10{}: {C:acid}+0{}", "{C:acid}10-19{}: {C:acid}+2{}", "{C:acid}20-29{}: {C:acid}+4{}", "{C:acid}>29{}: {C:acid}+6{}", "{C:inactive}(Currently {C:acid}#1# Acidity{C:inactive}){}"}
+				text = {"Increases {C:acid}Acidity{} of all other", "{C:acid}Acids{} based on the current {C:acid}Acidity", "{C:acid}<10{}: {C:acid}+0", "{C:acid}10-19{}: {C:acid}+2", "{C:acid}20-29{}: {C:acid}+4", "{C:acid}>29{}: {C:acid}+6", "{C:inactive}(Currently {C:acid}#1# Acidity{C:inactive})"}
 			},
 			c_bium_hbr = {
 				name = "Hydrobromic Acid",
-				text = {"Gives {C:money}money{} base on", "the current {C:acid}Acidity{}", "{C:acid}<5{}: {C:money}+$0{}", "{C:acid}5-14{}: {C:money}+$10{}", "{C:acid}15-29{}: {C:money}+$20{}", "{C:acid}>29{}: {C:money}+$30{}", "{C:inactive}(Currently {C:acid}#1# Acidity{C:inactive}){}"}
+				text = {"Gives {C:money}money{} base on", "the current {C:acid}Acidity", "{C:acid}<5{}: {C:money}+$0", "{C:acid}5-14{}: {C:money}+$10", "{C:acid}15-29{}: {C:money}+$20", "{C:acid}>29{}: {C:money}+$30", "{C:inactive}(Currently {C:acid}#1# Acidity{C:inactive})"}
 			}
 		},
 		Other = {
